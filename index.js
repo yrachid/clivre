4b076a7431b713ff12152ba79a8ed271390fd756
othman853 <anuar.yasser@gmail.com>
2017-11-16 16:03:47 -0200

    Adds logs to mongo connection events

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose')
 const app = require('./src/app')
 
 const DB_URL = process.env.CLIVRE_DB_URL || 'mongodb://localhost:27017/clivre'
-// const DB_URL = 'mongodb://localhost:27017/clivre'
 
 mongoose.Promise = Promise
+
+mongoose.connection.on('connected', () => console.log('[MONGO] Connected'))
+mongoose.connection.on('error', () => console.log('[MONGO] Error'))
+mongoose.connection.on('disconnected', () => console.log('[MONGO] Disconnected'))
 
 mongoose
   .connect(DB_URL, {useMongoClient: true})
